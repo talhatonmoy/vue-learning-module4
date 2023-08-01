17ac08893a574ce51a1ecc63c9386e9a64f6f0dd
talhatonmoy <talha.tonmoy.ck@gmail.com>
2023-08-01 13:05:52 +0600

    pay with checkbox

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,22 @@
 <script setup>
 import { ref, reactive } from 'vue'
 
-const person = reactive({
-  name: 'Jon Deo',
-  age: '25',
-  title: 'CEO',
-  company: 'Marvel Cenematic Universe'
-});
+const checkboxInput = ref(true)
 
 </script>
 
 <template>
   <div class="container mx-auto w-full">
-    <h2 class="text-4xl capitalize mb-5">Person Detail</h2>
-    <p>{{ person }}</p>
+    <h2 class="text-4xl capitalize mb-5">Random Image Showing Based On Checbox Input </h2>
+    <p class="capitalize">{{ checkboxInput }}</p>
     <!-- Input Box -->
-    <div class=" bg-gray-200 p-5 rounded-lg mt-5">
-      <div v-for="(value, key, index) in person" :key="index" class="mt-5 text-left">
-        <label class="text-lg block mb-1 capitalize" for="color">{{ key }}:</label>
-        <input type="text" v-model="person[key]" id="color" class="p-1 px-2 w-full rounded-md focus:outline-none">
+    <div class=" bg-gray-100 p-5 rounded-lg mt-5 min-h-screen">
+      <div class=" bg-white p-5 rounded-md ">
+        <label class="text-lg capitalize mr-3">Show image randomly:</label>
+        <input type="checkbox" v-model="checkboxInput">
+      </div>
+      <div class="mt-5 text-left" v-show="checkboxInput == true">
+        <img :src="`https://source.unsplash.com/random?version=${Math.random()}`" class="w-6/12 mx-auto rounded-md">
       </div>
     </div>
 
